0cc19d046bc181a5fdd06c7f4ebc1e8b63dc2282
Danilo Grieco <grieco.danilo@gmail.com>
2023-04-12 00:16:58 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+%w(Atalanta Bologna Cremonese Empoli Fiorentina Inter Juventus Lazio Lecce Milan Monza Napoli Roma Salernitana
+Sampdoria Sassuolo Spezia Torino Udinese Verona).each do |t|
+  Team.create(name: t)
+end
+["2022/23"].each do |year|
+  38.times do |i|
+    Matchday.create(sequence_number: i+1, match_years: year)
+  end
+end
